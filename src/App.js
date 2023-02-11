20cdaad2914c62f4883eb9e9632d061c8b06bdae
Ranamasud1414 <ranamasud1052002@gmail.com>
2023-02-12 01:44:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+// import About from './components/About';
+import Alert from "./components/Alert";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+//   Link,
+// } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+// import Action from './components/Action';
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggle = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode anable", "success ");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Day mode anable", "success");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <BrowserRouter> */}
+      <Navbar title="Alibrothers" mode={mode} toggle={toggle} />
+      <Alert alert={alert} />
+      <div className="container">
+        {/* <Routes> */}
+        {/* <Route path='about' element={<About mode={mode}/>} ></Route> */}
+        {/* <Route path='/' element={  */}
+        <TextForm showAlert={showAlert} heading="Enter Your Text" mode={mode} />
+
+        {/* </Route> */}
+        {/* <Route path='action' element={ <Action mode={mode}/>} ></Route> */}
+        {/* </Routes> */}
+      </div>
+
+      {/* </BrowserRouter> */}
+    </>
   );
 }
 
